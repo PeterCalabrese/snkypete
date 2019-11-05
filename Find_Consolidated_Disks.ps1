be079988803b42610c3fc096add11a7102553de7
PeterCalabrese <57399781+PeterCalabrese@users.noreply.github.com>
2019-11-05 09:54:26 -0500

    Delete Find_Consolidated_Disks.ps1

--- Find_Consolidated_Disks.ps1
+++
@@ -1,3 +0,0 @@
-Get-VM | Where-Object -Property Name -Like "*cma*" | Get-HardDisk | Where-Object -Property FileName -NotLike "*cma*" | Select-Object -Property Parent ,FileName 
-
-#| Export-Csv H:\scripts\CHG0042328_Disk_Consolidation.csv -NoTypeInformation -UseCulture
